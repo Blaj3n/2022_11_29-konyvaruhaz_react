1aa9b6412f0798dd257a847cd2bcf8b4a57e57bc
Blaj3n <benceulrich@gmail.com>
2022-11-29 13:58:40 +0100

    prettier

--- konyvaruhaz/src/Kosar.js
+++ konyvaruhaz/src/Kosar.js
@@ -1,14 +1,12 @@
-function Kosar(props){
-
-
-    return(
-        <tr>
-            <td>{props.kosar.szerzo}</td>
-            <td>{props.kosar.cim}</td>
-            <td>{props.kosar.ar}</td>
-            <td>{props.kosar.db}</td>
-        </tr>
-    );
+function Kosar(props) {
+  return (
+    <tr>
+      <td>{props.kosar.szerzo}</td>
+      <td>{props.kosar.cim}</td>
+      <td>{props.kosar.ar}</td>
+      <td>{props.kosar.db}</td>
+    </tr>
+  );
 }
 
 export default Kosar;
